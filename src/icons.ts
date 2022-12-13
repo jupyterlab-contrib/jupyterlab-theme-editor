f3deafc232c4f0520f29fb439dd24ff93157c7c8
Florence Haudin <haudin.florence@gmail.com>
2022-12-13 10:57:11 +0100

    Update dependencies, remove unused icons and take into accounts the other review suggestions.

--- src/icons.ts
+++ src/icons.ts
@@ -1,5 +1,5 @@
 import { LabIcon } from '@jupyterlab/ui-components';
-import themeEditorSvgstr from '../style/icons/theme-editor1.svg';
+import themeEditorSvgstr from '../style/icons/theme-editor.svg';
 
 export const themeEditorIcon = new LabIcon({
   name: '@jupyterlab/jupyter-theme-editor:theme-editor',
