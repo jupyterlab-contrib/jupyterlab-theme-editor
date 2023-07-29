8da1c4e055796ef944f171c7572c809fa578b482
Frederic COLLONVAL <fcollonval@users.noreply.github.com>
2023-07-29 16:54:59 +0200

    Built on top of copier template

--- jupyter_theme_editor/__init__.py
+++ jupyter_theme_editor/__init__.py
@@ -1,6 +1,13 @@
-from ._version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    # Fallback when using the package in dev mode without installing
+    # in editable mode with pip. It is highly recommended to install
+    # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
+    import warnings
+    warnings.warn("Importing 'jupyter_theme_editor' outside a proper installation.")
+    __version__ = "dev"
 from .handlers import setup_handlers
-
 
 
 def _jupyter_labextension_paths():
@@ -8,7 +15,6 @@ def _jupyter_labextension_paths():
         "src": "labextension",
         "dest": "jupyter-theme-editor"
     }]
-
 
 
 def _jupyter_server_extension_points():
@@ -32,4 +38,3 @@ def _load_jupyter_server_extension(server_app):
 
 # For backward compatibility with notebook server - useful for Binder/JupyterHub
 load_jupyter_server_extension = _load_jupyter_server_extension
-
